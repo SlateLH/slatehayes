4a25633fd93f45a7c5a7bfe005751be0f953e379
Slate Hayes <slatelhayes@gmail.com>
2024-03-08 14:33:54 -0300

    Add GitHub mark and link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -34,16 +35,31 @@ export default function Home() {
               <Link href="tel:+16602271430">{"+1 (660) 227-1430 (USA)"}</Link>
             </div>
           </div>
-          <Link className="flex items-center gap-2" href="mailto:slatelhayes@gmail.com">
-            <span className="flex items-center gap-1">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-              </svg>
-              Email:
-            </span>
-            slatelhayes@gmail.com
-          </Link>
+          <div className="flex flex-col gap-1">
+            <Link className="flex items-center gap-2" href="mailto:slatelhayes@gmail.com">
+              <span className="flex items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                  <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                  <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                </svg>
+                Email:
+              </span>
+              slatelhayes@gmail.com
+            </Link>
+            <Link className="flex items-center gap-2" href="https://github.com/SlateLH">
+              <span className="flex items-center gap-1">
+                <Image
+                  src="/github-mark.svg"
+                  alt="GitHub mark"
+                  width={16}
+                  height={16}
+                  priority
+                />
+                GitHub:
+              </span>
+              SlateLH
+            </Link>
+          </div>
         </div>
         <div className="w-full flex flex-col pt-8">
           <div className="flex items-center gap-1 border-b-4">

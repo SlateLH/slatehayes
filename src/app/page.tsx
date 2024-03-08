ed27023875f2e1c10153d4212feb24cde3b4e2b1
Slate Hayes <slatelhayes@gmail.com>
2024-03-08 11:30:26 -0300

    Add links to send emails and call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
           </span>
         </div>
         <div className="w-full flex items-center justify-between pt-1">
-          <div className="flex items-center gap-2">
+          <Link className="flex items-center gap-2" href="tel:+5554996962828" title="Click to call">
             <span className="flex items-center gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                 <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
@@ -22,8 +23,8 @@ export default function Home() {
               Phone:
             </span>
             {"+55 (54) 99696-2828"}
-          </div>
-          <div className="flex items-center gap-2">
+          </Link>
+          <Link className="flex items-center gap-2" href="mailto:slatelhayes@gmail.com" title="Click to send an email">
             <span className="flex items-center gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                 <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
@@ -32,7 +33,7 @@ export default function Home() {
               Email:
             </span>
             slatelhayes@gmail.com
-          </div>
+          </Link>
         </div>
     </main>
   );
